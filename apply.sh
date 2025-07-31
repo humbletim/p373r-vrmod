b7c3354c1da3472ad2917d1122ed2d632855fa0a
humbletim <humbletim@gmail.com>
2025-07-31 10:07:48 -0400

    Delete apply.sh

--- apply.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-test -d "$source_dir" || { echo "!source_dir" >&2; exit 1; }
-
-here="$(readlink -f $(dirname $0))"
-cd $source_dir/..
-grep P373R llviewerdisplay.cpp >/dev/null || (
-  patch --ignore-whitespace --verbose --merge -p1 < $here/20240331.diff.U.patch
-)
-
