d97aaf6d4684c67d79453ce91d533549a8a4efc5
humbletim <humbletim@gmail.com>
2025-08-04 16:32:34 -0400

    fix paths and mesa

--- experiments/portable-opengl32-mesa.bat
+++ experiments/portable-opengl32-mesa.bat
@@ -3,7 +3,7 @@ set MESA_VER=24.3.2
 
 echo [portable-opengl32-mesa version=%MESA_VER%]
 if exist bin\opengl32.dll (
-    echo -- skipping (bin\opengl32.dll already exists)
+    echo -- skipping (bin\opengl32.dll already exists^)
     goto :done
 )
 
@@ -15,7 +15,8 @@ if not exist tmp\mesa-llvmpipe-x64-%MESA_VER%.zip (
 )
 echo -- unpacking mesa-llvmpipe
 if not exist bin (mkdir bin)
-  %SYSTEMROOT%\System32\tar.exe -Cbin -xvf tmp\mesa-llvmpipe-x64-%MESA_VER%.zip
+
+%SYSTEMROOT%\System32\tar.exe -Cbin -xvf tmp\mesa-llvmpipe-x64-%MESA_VER%.zip
 if errorlevel 1 (echo error unpacking mesa-opengl32 && exit /b 1)
 
 :done
