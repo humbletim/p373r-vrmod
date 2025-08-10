c6277b2ff0d66d9306cf4639da573043fc23602e
humbletim <humbletim@gmail.com>
2025-08-10 17:35:37 -0400

    fix .rsp refs

--- experiments/fs/setup.bat
+++ experiments/fs/setup.bat
@@ -16,6 +16,12 @@ if not exist fs-7.1.13-devtime-avx2 ( echo missing fs-7.1.13-devtime-avx2 && exi
 
 set snapshot_dir=fs-7.1.13-devtime-avx2
 set devtime=fs-devtime
+
+if not exist %devtime% ( mkdir %devtime% )
+
+envsubst.exe < %snapshot_dir%/llobjs.rsp.in > %devtime%/llobjs.rsp
+envsubst.exe < %snapshot_dir%/llincludes.rsp.in > %devtime%/llincludes.rsp
+
 envsubst.exe < %~dp0/devtime.in/compile.rsp > %devtime%/compile.rsp
 envsubst.exe < %~dp0/devtime.in/link.rsp > %devtime%/link.rsp
 envsubst.exe < %~dp0/devtime.in/custom-objs.rsp > %devtime%/custom-objs.rsp
