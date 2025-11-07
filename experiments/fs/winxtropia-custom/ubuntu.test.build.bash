7738efb7ba9cba40cb4cbf08379d8a4c023d5a55
tim <humbletim@gmail.com>
2025-11-07 18:18:57 -0500

    customizing test

--- experiments/fs/winxtropia-custom/ubuntu.test.build.bash
+++ experiments/fs/winxtropia-custom/ubuntu.test.build.bash
@@ -6,6 +6,7 @@ test -v snapshot_dir
 test -x experiments/fs/setup.bash && test ! -d p373r-vrmod-devtime && ln -s . p373r-vrmod-devtime || true
 bash ./p373r-vrmod-devtime/experiments/fs/setup.bash
 test -s fs-devtime/env
+mkdir -pv build
 cp -av ./p373r-vrmod-devtime/experiments/fs/winxtropia-custom/placeholder-icon.ico build/ -v
 bash fs-devtime/env ./p373r-vrmod-devtime/experiments/fs/winxtropia-custom/cosmetics.bash 
 bash fs-devtime/env ./p373r-vrmod-devtime/experiments/fs/winxtropia-custom/build.bash 
