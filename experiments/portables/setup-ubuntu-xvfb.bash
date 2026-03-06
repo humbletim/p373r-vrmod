f1474315bc84decafa9bcde822695878d21b880a
tim <humbletim@gmail.com>
2026-03-06 16:39:19 -0500

    refactoring

--- experiments/portables/setup-ubuntu-xvfb.bash
+++ experiments/portables/setup-ubuntu-xvfb.bash
@@ -3,7 +3,6 @@ set -euo pipefail
 
 echo "--- installing ubuntu xvfb/wine deps ---"
 sudo apt-get update 
-sudo apt-get install -qq -y 
-sudo apt install -y -qq -o=Dpkg::Use-Pty=0 wine-stable xvfb fluxbox imagemagick x11-apps
+sudo DEBIAN_FRONTEND=noninteractive apt install -y -qq -o=Dpkg::Use-Pty=0 wine-stable xvfb fluxbox imagemagick x11-apps
 
 which wine
