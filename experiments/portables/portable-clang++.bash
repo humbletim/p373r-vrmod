ea590d64cc15621f2cf04da80bf717b848835694
tim <humbletim@gmail.com>
2026-03-06 16:54:48 -0500

    refactoring

--- experiments/portables/portable-clang++.bash
+++ experiments/portables/portable-clang++.bash
@@ -4,7 +4,8 @@ set -euo pipefail
 
 if apt-cache show clang-19 > /dev/null 2>&1 ; then
     echo "--- installing LLVM/Clang 19 via apt ---"
-    sudo apt-get install -qq -y clang-19 lld-19 
+    export DEBIAN_FRONTEND=noninteractive
+    sudo apt-get install -y -qq -o=Dpkg::Use-Pty=0 clang-19 lld-19 
 else
     echo "--- installing LLVM/Clang 19 via llvm.sh ---"
     wget -O /tmp/llvm.sh https://apt.llvm.org/llvm.sh
